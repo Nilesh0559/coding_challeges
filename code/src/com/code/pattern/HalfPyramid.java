f732f95c22aea8580efbf486906a272a3d07afa2
Nilesh0559 <nileshgajendragadkar@gmail.com>
2023-12-31 11:15:33 +0530

    Pattern Printing - Half Pyramid

---
+++ code/src/com/code/pattern/HalfPyramid.java
@@ -0,0 +1,25 @@
+package com.code.pattern;
+
+/*******************************************************************
+* Author: Nilesh Gajendragadkar
+* Topic : Pattern Printing
+* Half-Pyramid
+* 	*
+*	**
+*	***
+*	****
+*	*****
+*	******
+*******************************************************************/
+
+public class HalfPyramid {
+
+	public static void main(String[] args) {
+		int rows = 6;
+		for(int i = 1; i <= rows; i++) {
+			for(int j = 1; j <= i; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+}
