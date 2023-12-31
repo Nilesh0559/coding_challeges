d8c0907361f0b194ccb2609bfa4d5c452cc844d2
Nilesh0559 <nileshgajendragadkar@gmail.com>
2023-12-31 23:52:54 +0530

    Pattern Printing - Numbers Full Pyramid

---
+++ code/src/com/code/pattern/numbers/FullPyramid.java
@@ -0,0 +1,31 @@
+package com.code.pattern.numbers;
+
+/*******************************************************************
+* Author: Nilesh Gajendragadkar
+* Topic : Pattern Printing
+* Full Pyramid
+* 	          1           
+*	        2 3 2         
+*	      3 4 5 4 3       
+*	    4 5 6 7 6 5 4     
+*	  5 6 7 8 9 8 7 6 5   
+*******************************************************************/
+
+public class FullPyramid {
+
+	public static void main(String[] args) {
+		int i, j, k, l = 1;
+		for(j = 1; j <= 5; j++) {
+			for(i = j; i <= 5; i++) 
+				System.out.print("  ");
+			for(k = j; k <= ((j * 2) - 1) ; k++)
+				System.out.print(k + " ");
+			for(k = ((j-1) * 2); k >= j; k--)
+				System.out.print(k + " ");
+			for(i = j; i <= 5; i++) 
+				System.out.print("  ");
+			System.out.println();
+		}
+		
+	}
+}
