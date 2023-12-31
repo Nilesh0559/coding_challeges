2a670690930eeeabc06343d31a60cfa9d9c3c357
Nilesh0559 <nileshgajendragadkar@gmail.com>
2023-12-31 13:23:28 +0530

    Pattern Printing - Hollow Full Pyramid

---
+++ code/src/com/code/pattern/HollowFullPyramid.java
@@ -0,0 +1,32 @@
+package com.code.pattern;
+
+/*******************************************************************
+* Author: Nilesh Gajendragadkar
+* Topic : Hollow Full Pyramid
+* 	      *       
+*	     * *      
+*	    *   *     
+*	   *     *    
+*	  *       *   
+*	 * * * * * *  
+*******************************************************************/
+
+public class HollowFullPyramid {
+
+	public static void main(String[] args) {
+		int i, j, rows = 6;
+		for(j = 1; j <= rows; j++) {
+			for(i = j; i <= rows; i++)
+				System.out.print(" ");
+			for(i = 1; i <= j; i++) {
+				if(i == 1 || j == rows || i == j)
+					System.out.print("* ");
+				else
+					System.out.print("  ");
+			}
+			for(i = j; i <= rows; i++)
+				System.out.print(" ");
+			System.out.println();
+		}
+	}
+}
