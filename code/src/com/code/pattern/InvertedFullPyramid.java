25310fb584ed025ec9f00faac063059ffaa2c472
Nilesh0559 <nileshgajendragadkar@gmail.com>
2023-12-31 12:53:32 +0530

    Pattern Printing - Inverted Full Pyramid

---
+++ code/src/com/code/pattern/InvertedFullPyramid.java
@@ -0,0 +1,29 @@
+package com.code.pattern;
+
+/*******************************************************************
+* Author: Nilesh Gajendragadkar
+* Topic : Pattern Printing
+* Inverted Full Pyramid
+* 	 * * * * * *  
+*	  * * * * *   
+*	   * * * *    
+*	    * * *     
+*	     * *      
+*	      *       
+*******************************************************************/
+
+public class InvertedFullPyramid {
+
+	public static void main(String[] args) {
+		int i, j, k, rows = 6;
+		for(j = 1; j <= rows; j++) {
+			for(i = 1; i <= j; i++)
+				System.out.print(" ");
+			for(k = rows-j+1; k >= 1; k--)
+				System.out.print("* ");
+			for(i = 1; i <= j; i++)
+				System.out.print(" ");
+			System.out.println();
+		}
+	}
+}
